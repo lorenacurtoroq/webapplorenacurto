389112e8c7d0ee39be0646c85ea78b0086c7d558
lorenacurtoroq <168084488+lorenacurtoroq@users.noreply.github.com>
2024-05-20 13:44:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ function geoExit(posicio){
     }
 }
 async function inicia_video() {
-    const codi_model = "*********"    // substitueix els asteriscs pel codi del model d'IA que vas crear en una activitat anterior
+    const codi_model = "CiG107DfP"    // substitueix els asteriscs pel codi del model d'IA que vas crear en una activitat anterior
     const tmURL = "https://teachablemachine.withgoogle.com/models/" + codi_model;
     const modelURL = tmURL + "/model.json";
     const metadataURL = tmURL + "/metadata.json";
